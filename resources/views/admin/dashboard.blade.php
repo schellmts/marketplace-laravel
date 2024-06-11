194f412ae32d02eba566ed3c2612c8af5383f3bc
schellmts <matheus.emiliano@hotmail.com>
2024-06-11 16:31:31 -0300

    otimizacao rotas

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Painel Administrador</title>
+</head>
+
+<body>
+    <h1>painel</h1>
+    <p>tamo adm</p>
+    <div>{{ Auth::user()->name }}</div>
+    <a href="/profile">perfil</a>
+
+
+</body>
+
+</html>
---
+++ routes/web/vendor.php
@@ -0,0 +1,6 @@
+<?php
+
+use App\Http\Controllers\Backend\VendorController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('vendor/dashboard', [VendorController::class, 'dashboard'])->middleware(['auth', 'vendor'])->name('vendor.dashboard');
